4f0998386e27f3d18b6ddee8a50762e80b04a990
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2026-09-21 12:32:04 +0530

    feat: implement error handling and response utilities with ApiError, errorHandler, asyncHandler, validate middleware, and ApiResponse class

---
+++ backend/src/core/errors/errorHandler.js
@@ -0,0 +1,27 @@
+import ApiError from "./ApiError.js";
+
+const errorHandler = (err, req, res, next) => {
+  let error = err;
+
+  if (!(error instanceof ApiError)) {
+    const statusCode = error.statusCode || error.status || 500;
+
+    const message = error.message || "Internal Server Error";
+
+    error = new ApiError(statusCode, message, error.errors || [], error.stack);
+  }
+
+  const response = {
+    success: false,
+    message: error.message,
+    errors: error.errors,
+  };
+
+  if (process.env.NODE_ENV === "development") {
+    response.stack = error.stack;
+  }
+
+  return res.status(error.statusCode).json(response);
+};
+
+export default errorHandler;
